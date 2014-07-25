fae9a093ef6dd3cc083d59875a398d8d8fbc26f3
mzkd @ archi.local <contact@studiomzk.com>
2014-07-25 18:51:21 +0200

    Tweaks with time into app.js for more visual fx, 3 new sections into DOM...

--- js/app.js
+++ js/app.js
@@ -4,12 +4,11 @@
 
 	$('.frame').data('img', value).each(function(index){
 		imgUrl = $(this).attr('data-img');
-		console.log(index + " = " + imgUrl + " | browser width = " + browserW);
-
+		// console.log(index + " = " + imgUrl + " | browser width = " + browserW);
 
 		$(this).css({
-					background: 'url(' + imgUrl + ')',
-					});
+			background: 'url(' + imgUrl + ')',
+			});
 
 	});
 
@@ -17,20 +16,20 @@
         function () {
             var $this = $(this);
             // $('.frame').not($this).css('display','none');
-            $('.frame').not($this).stop().animate({width: '0%'}, 500);
+            $('.frame').not($this).stop().animate({width: '0%'}, 400);
 
-            $this.stop().animate({width:'100%'},500);
+            $this.stop().animate({width:'100%'},400);
             $('.heading',$this).stop(true,true).fadeOut();
-            $('.bgDescription',$this).stop(true,true).slideDown(500);
+            $('.bgDescription',$this).stop(true,true).slideDown(800);
             $('.description',$this).stop(true,true).fadeIn();
         },
         function () {
             var $this = $(this);
-            $('.frame').not($this).stop().animate({width:'33.33%'}, 500);
-            $this.stop().animate({'width':'33.33%'},1000);
+            $('.frame').not($this).stop().animate({width:'33.33%'}, 800);
+            $this.stop().animate({'width':'33.33%'},800);
             $('.heading',$this).stop(true,true).fadeIn();
-            $('.description',$this).stop(true,true).fadeOut(500);
-            $('.bgDescription',$this).stop(true,true).slideUp(700);
+            $('.description',$this).stop(true,true).fadeOut(450);
+            $('.bgDescription',$this).stop(true,true).slideUp(250);
         }
     );
 
