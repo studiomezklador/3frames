0088776eaa07c979e3c19ca4239a40e4ad628a2b
mzkd @ archi.local <contact@studiomzk.com>
2014-07-25 17:46:18 +0200

    Few fixes on CSS + JS

--- js/app.js
+++ js/app.js
@@ -16,14 +16,18 @@
 	$('#accordion > li').hover(
         function () {
             var $this = $(this);
-            $this.stop().animate({'width':'68%'},500);
+            // $('.frame').not($this).css('display','none');
+            $('.frame').not($this).stop().animate({width: '0%'}, 500);
+
+            $this.stop().animate({width:'100%'},500);
             $('.heading',$this).stop(true,true).fadeOut();
             $('.bgDescription',$this).stop(true,true).slideDown(500);
             $('.description',$this).stop(true,true).fadeIn();
         },
         function () {
             var $this = $(this);
-            $this.stop().animate({'width':'15%'},1000);
+            $('.frame').not($this).stop().animate({width:'33.33%'}, 500);
+            $this.stop().animate({'width':'33.33%'},1000);
             $('.heading',$this).stop(true,true).fadeIn();
             $('.description',$this).stop(true,true).fadeOut(500);
             $('.bgDescription',$this).stop(true,true).slideUp(700);
